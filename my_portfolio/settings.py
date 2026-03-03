b22ddaa555fccd41620d6491c2dd6917ff2825fe
Ali Hassan <my.dev2509@gmail.com>
2026-03-03 17:08:12 +0500

    Fix AttributeError by re-adding legacy storage settings for Django 5.2

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -120,14 +120,17 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 CLOUDINARY_STORAGE = {
         'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
         'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+        'SECURE': True,
+
     }
+
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
@@ -137,6 +140,8 @@ STORAGES = {
     },
 }
 
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/' 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
