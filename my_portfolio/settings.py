ac35c47ebaae0c91b98db3075e008f0083a68b61
Ali Hassan <my.dev2509@gmail.com>
2026-03-03 11:19:44 +0500

    Fix middleware order and force upload of static/media folders

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -36,14 +36,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Move it here, right after Security
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'my_portfolio.urls'
 
